77ff6255fd49f54c4cf900f11830ccb3cb668846
hisomura <hisomura.noreply@gmail.com>
2020-08-01 16:11:39 +0900

    レクチャー144まで

---
+++ src/components/project-input.ts
@@ -0,0 +1,60 @@
+/// <reference path="base-components.ts" />
+/// <reference path="../decorators/auto-bind.ts" />
+/// <reference path="../models/project.ts" />
+/// <reference path="../models/drag-drop.ts" />
+
+namespace App {
+  export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
+    titleInputElement: HTMLInputElement
+    descriptionInputElement: HTMLInputElement
+    mandayInputElement: HTMLInputElement
+
+    constructor() {
+      super('project-input', 'app', true, 'user-input')
+      this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement
+      this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement
+      this.mandayInputElement = this.element.querySelector('#manday') as HTMLInputElement
+      this.configure()
+    }
+
+    private gatherUserInput(): [string, string, number] | void {
+      const enteredTitle = this.titleInputElement.value
+      const enteredDescription = this.descriptionInputElement.value
+      const enteredManday = this.mandayInputElement.value
+
+      const titleValidatable: Validatable = { value: enteredTitle }
+      const descriptionValidatable: Validatable = { value: enteredDescription, required: true, minLength: 5 }
+      const mandayValidatable: Validatable = { value: +enteredManday, required: true, min: 1, max: 1000 }
+
+      if (!validate(titleValidatable) || !validate(descriptionValidatable) || !validate(mandayValidatable)) {
+        alert('入力値が正しくありません')
+        return
+      } else {
+        return [enteredTitle, enteredDescription, +enteredManday]
+      }
+    }
+
+    private clearInput() {
+      this.titleInputElement.value = ''
+      this.descriptionInputElement.value = ''
+      this.mandayInputElement.value = ''
+    }
+
+    @autoBind
+    private submitHandler(event: Event) {
+      event.preventDefault()
+      const userInput = this.gatherUserInput()
+      if (Array.isArray(userInput)) {
+        const [title, desc, manday] = userInput
+        projectState.addProject(title, desc, manday)
+        this.clearInput()
+      }
+    }
+
+    protected renderContent() {}
+
+    protected configure() {
+      this.element.addEventListener('submit', this.submitHandler.bind(this))
+    }
+  }
+}
