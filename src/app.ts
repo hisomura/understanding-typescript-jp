1b0bc009153399fe9d2e8946d32253a0dc1636f8
hisomura <hisomura.noreply@gmail.com>
2020-08-05 19:58:43 +0900

    レクチャー173まで APIキー無効化済み

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,39 @@
-import 'reflect-metadata';
-import { plainToClass } from "class-transformer";
-import { Product } from './product.model'
-import {validate} from "class-validator";
+import axios from 'axios'
 
-const products = [
-  { title: '商品1', price: 100 },
-  { title: '商品2', price: 200 },
-]
+const form = document.querySelector('form')!
+const addressInput = document.getElementById('address')! as HTMLInputElement
 
-const p1 = new Product('商品1', 100)
-console.log(p1.getInformation())
+const GOOGLE_API_KEY = 'AIzaSyDVrRgTnS_R_kbj9ry5_1mv9PlxqgJ8NQY'
 
-
-const loadedProducts = plainToClass(Product, products)
-
-for (const p of loadedProducts) {
-  console.log(p.getInformation())
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[]
+  status: 'OK' | 'ZERO_RESULTS'
 }
 
-const newProd = new Product('', -100)
-validate(newProd).then(errors => {
-  if (errors.length > 0) {
-    console.log('バリデーションエラー', errors)
-  } else {
-    console.log(newProd.getInformation())
-  }
-})
+function searchAddressHandler(event: Event) {
+  event.preventDefault()
+  const enteredAddress = addressInput.value
+
+  const parameters = `address=${encodeURI(enteredAddress)}&key=${GOOGLE_API_KEY}`
+  axios
+    .get<GoogleGeocodingResponse>('https://maps.googleapis.com/maps/api/geocode/json?' + parameters)
+    .then((response) => {
+      if (response.data.status !== 'OK') {
+        throw new Error('座標を取得できませんでした')
+      }
+      const coordinates = response.data.results[0].geometry.location
+      const map = new google.maps.Map(document.getElementById('map') as HTMLElement, {
+        center: coordinates,
+        zoom: 16,
+      })
+      new google.maps.Marker({position: coordinates, map: map});
+
+      console.log(response)
+    })
+    .catch((err) => {
+      alert(err.message)
+      alert(err)
+    })
+}
+
+form.addEventListener('submit', searchAddressHandler)
