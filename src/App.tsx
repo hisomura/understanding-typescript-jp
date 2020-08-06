1c4d4c26ced5ab56456e9c58528214aa3d9c1294
hisomura <hisomura.noreply@gmail.com>
2020-08-07 07:53:20 +0900

    レクチャー186まで

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
+import TodoList from "./components/TodoList";
+import NewTodo from "./components/NewTodo";
+
+type Todo = {
+  id: string,
+  text: string
+}
 
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([])
+  const todoAddHandler = (text: string) => {
+    setTodos(prevTodos => [...prevTodos, { id: Math.random().toString(), text }])
+  }
+  const todoDeleteHandler = (todoId: string) => {
+    setTodos(prevTodos => prevTodos.filter(todo => todo.id !== todoId))
+  }
+
   return (
-    <div className="App"></div>
+    <div className="App">
+      <NewTodo onAddTodo={todoAddHandler}/>
+      <TodoList items={todos} onDeleteTodo={todoDeleteHandler}/>
+    </div>
   );
 }
 
